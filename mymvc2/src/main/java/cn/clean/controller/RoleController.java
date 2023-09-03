fecd60198fcc3608085768d8d1f9e1fe62ee535a
jiruixin <1148769892@qq.com>
2023-09-03 16:54:21 +0800

    add springmvc not responsebody

---
+++ mymvc2/src/main/java/cn/clean/controller/RoleController.java
@@ -0,0 +1,13 @@
+package cn.clean.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RoleController {
+
+    @GetMapping("/index")
+    public String role(){
+        return "index";
+    }
+}
