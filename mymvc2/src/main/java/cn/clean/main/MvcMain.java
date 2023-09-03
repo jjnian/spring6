71f81df7eb2f381f8902e1a1376d345758307618
ji_rxin <1148769892@qq.com>
2023-09-03 12:50:10 +0800

    add springmvc

---
+++ mymvc2/src/main/java/cn/clean/main/MvcMain.java
@@ -0,0 +1,12 @@
+package cn.clean.main;
+
+import cn.clean.app.SpringApplication;
+
+/**
+ *
+ */
+public class MvcMain {
+	public static void main(String[] args) {
+		SpringApplication.run();
+	}
+}
